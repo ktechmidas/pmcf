ff2f2f70ee66166f15a761f4e276aba903d2c85a
Stephen Gran <stephen.gran@piksel.com>
2014-04-24 17:09:15 +0100

    catch error exits
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+ret=0
+
 echo "Running unit tests ..."
-nosetests -v
+nosetests -v || ret=$(($ret|$?))
 
 echo "Running pep8 tests ..."
-pep8 pmcf
-pep8 tests
+pep8 --show-source pmcf || ret=$(($ret|$?))
+pep8 --show-source tests || ret=$(($ret|$?))
+
+exit $ret
