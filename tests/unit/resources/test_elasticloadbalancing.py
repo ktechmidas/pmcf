8e242b6659f7a0a592a1cbf743ae51a56b4fd2d9
Stephen Gran <steve@lobefin.net>
2014-05-11 20:48:54 +0100

    drop unused import
    Signed-off-by: Stephen Gran <steve@lobefin.net>

--- tests/unit/resources/test_elasticloadbalancing.py
+++ tests/unit/resources/test_elasticloadbalancing.py
@@ -12,7 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import json
 from nose.tools import assert_equals, assert_raises
 
 from pmcf.exceptions import PropertyException
