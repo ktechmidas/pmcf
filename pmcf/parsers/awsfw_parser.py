f54f6dfeb1ce36badcf95cbd82e2735d40eb89bb
Stephen Gran <stephen.gran@piksel.com>
2014-05-12 17:45:03 +0100

    add start of awsfw parser
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

---
+++ pmcf/parsers/awsfw_parser.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2014 Piksel
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from pmcf import exceptions
+from pmcf.parsers import BaseParser
+from pmcf.utils import xmltodict
+
+
+class AWSFWParser(BaseParser):
+
+    @staticmethod
+    def _listify(data):
+        if not isinstance(data, list):
+            return [data]
+        return data
+
+    @staticmethod
+    def _build_hc(hc_xml):
+        (protocol, rest) = hc_xml.split(':')
+        if protocol.upper() in ['HTTP', 'HTTPS']:
+            port = rest.split('/')[0]
+            path = rest[len(port):]
+        else:
+            port = rest
+            path = None
+        hc = {
+            'protocol': protocol.upper(),
+            'port': port
+        }
+        if path:
+            hc['path'] = path
+        return hc
+
+    def build_ds(self, ds):
+        elbs = self._listify(ds['ELB'])
+        for idx, elb in enumerate(elbs):
+            lb = {}
+            lb['listener'] = []
+            for listener in self._listify(elbs[idx]['listener']):
+                hc = listener.get('healthCheck',
+                                  listener['protocol'] + ':' +
+                                  listener['port'])
+                lb['listener'].append({
+                    'protocol': listener['protocol'],
+                    'lb_port': listener['port'],
+                    'instance_port': listener['instancePort'],
+                    'health_check': self._build_hc(hc),
+                })
+            self._resources['load_balancer'].append(lb)
+
+        instances = self._listify(ds['instances'])
+        for idx, instance in enumerate(instances):
+            inst = {}
+            inst['image'] = instance['amiId']
+            inst['type'] = instance['size']
+            inst['count'] = instance['count']
+            if instance.get('role') and instance.get('app'):
+                inst['provisioner'] = 'awsfw_standalone'
+                inst['apps'] = self._listify(instance['app'])
+                inst['roles'] = self._listify(instance['role'])
+
+            self._resources['instance'].append(inst)
+
+    def parse(self, config):
+        data = xmltodict.parse(config)
+        self.build_ds(data['c4farm'])
+        print self._resources
+        return self._resources
+
+
+__all__ = [
+    AWSFWParser
+]
---
+++ tests/unit/parsers/test_awsfw_parser.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2014 Piksel
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nose.tools import assert_equals, assert_raises
+
+from pmcf.parsers import awsfw_parser
+from pmcf.exceptions import ParserFailure
+
+
+class TestParser(object):
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_parse_valid_config(self):
+        struct = {
+            'foo': {
+                'bar': [1, 'three'],
+                'baz': True
+            }
+        }
+
+        config = ''
+        with open('data/su2c-v2.xml') as fd:
+            config = fd.read()
+
+        parser = awsfw_parser.AWSFWParser()
+        data = parser.parse(config)
+        # FIXME: Just to get a check in
+        assert_equals(data, data)
