0c05d917d6eadf471f5c3ee64ab75ee08fdb0118
Stephen Gran <stephen.gran@piksel.com>
2014-05-19 17:01:55 +0100

    hold onto lb object until we take a ref
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

--- pmcf/outputs/json_output.py
+++ pmcf/outputs/json_output.py
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from troposphere import Template
+from troposphere import Ref, Template
 
 from pmcf.outputs.base_output import BaseOutput
 from pmcf.resources.aws import *
@@ -33,21 +33,7 @@ class JSONOutput(BaseOutput):
                 value=config[tagkey]
             ))
 
-        config = {'foo': 'bar'}
-        for inst in resources['instance']:
-            for c in range(1, int(inst['count'])):
-                data.add_resource(ec2.Instance(
-                    'test%02d' % c,
-                    IamInstanceProfile='awsfw-provisioner',
-                    ImageId=inst['image'],
-                    InstanceType=inst['type'],
-                    KeyName=inst['sshKey'],
-                    Monitoring=inst['monitoring'],
-                    SecurityGroups=inst['sg'],
-                    Tags=tags,
-                    UserData=provisioner.userdata(config)
-                ))
-
+        lbs = {}
         for lb in resources['load_balancer']:
             lb_hc_tgt = lb['healthcheck']['protocol'] + ':' + \
                 lb['healthcheck']['port']
@@ -63,8 +49,9 @@ class JSONOutput(BaseOutput):
                     Protocol=listener['protocol']
                 ))
 
-            data.add_resource(elasticloadbalancing.LoadBalancer(
-                'test',
+            name = "ELB" + lb['name']
+            lbs[name] = elasticloadbalancing.LoadBalancer(
+                "ELB" + lb['name'],
                 CrossZone=True,
                 HealthCheck=elasticloadbalancing.HealthCheck(
                     'test',
@@ -75,7 +62,23 @@ class JSONOutput(BaseOutput):
                     UnhealthyThreshold=3
                 ),
                 Listeners=listeners
-            ))
+            )
+            data.add_resource(lbs[name])
+
+        config = {'foo': 'bar'}
+        for inst in resources['instance']:
+            for c in range(1, int(inst['count'])):
+                data.add_resource(ec2.Instance(
+                    'test%02d' % c,
+                    IamInstanceProfile='awsfw-provisioner',
+                    ImageId=inst['image'],
+                    InstanceType=inst['type'],
+                    KeyName=inst['sshKey'],
+                    Monitoring=inst['monitoring'],
+                    SecurityGroups=inst['sg'],
+                    Tags=tags,
+                    UserData=provisioner.userdata(config)
+                ))
         return data.to_json()
 
     def run(self, data):
