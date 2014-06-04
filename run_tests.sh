886c035f2676cb9f064c8a46b667e0456a96001b
Stephen Gran <stephen.gran@piksel.com>
2014-06-04 15:01:30 +0100

    switch to only using tox
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

--- run_tests.sh
+++ run_tests.sh
@@ -2,26 +2,10 @@
 
 ret=0
 
-tools_path=${tools_path:-$(dirname $0)/tools}
-venv_path=${venv_path:-${tools_path}}
-venv_dir=${venv_name:-/../.venv}
-TOOLS=${tools_path}
-VENV=${venv:-${venv_path}/${venv_dir}}
-
-test -d ${VENV} || ${TOOLS}/install_venv.sh
-
-source ${VENV}/bin/activate
-
 echo "Running unit tests ..."
 
 mkdir -p testreports
 
-tox -e py27,pep8
-nosetests "$@" || ret=$(($ret|$?))
-
-
-echo "Running pep8 tests ..."
-pep8 --show-source pmcf || ret=$(($ret|$?))
-pep8 --show-source tests || ret=$(($ret|$?))
+tox -e py27,pep8 || ret=$(($ret|$?))
 
 exit $ret
