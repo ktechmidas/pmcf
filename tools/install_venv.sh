87e1ee2e7880e11d179caa5d8593e131f536c155
Stephen Gran <stephen.gran@piksel.com>
2014-06-04 15:25:42 +0100

    we need the venv for building an artifact
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

--- tools/install_venv.sh
+++ tools/install_venv.sh
@@ -33,3 +33,8 @@ done
 if [ "${force}" = yes ];then
 	rm -rf .tox .venv coverage testreports
 fi
+
+virtualenv -q .venv
+tools/with_venv.sh pip install --upgrade 'pip>=1.4'
+tools/with_venv.sh pip install --upgrade setuptools
+tools/with_venv.sh pip install --upgrade -r ${PWD}/requirements.txt -r ${PWD}/test-requirements.txt
