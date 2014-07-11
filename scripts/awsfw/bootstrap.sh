51c8de6dd7f2943ef33a69317fd8bb5a44c60ee3
Francesco Ciocchetti <francesco.ciocchetti@piksel.com>
2014-07-11 16:58:55 +0100

    Update bootstrap and cloudconfig to properly bootstrap the instances

--- scripts/awsfw/bootstrap.sh
+++ scripts/awsfw/bootstrap.sh
@@ -3,6 +3,8 @@
 set -e
 set -u
 
+pip install --upgrade boto
+
 . /tmp/awsfw_vars
 
 /usr/local/lib/s3curl.pl --id ${AWS_ACCESS_KEY_ID} --key ${AWS_SECRET_ACCESS_KEY} -- -o /usr/local/bin/awsfw_dns.py https://aws-c4-003358414754.s3.amazonaws.com/bootstrap/awsfw_dns.py
@@ -17,7 +19,7 @@ chmod 0750 /etc/init.d/awsfw_dns
 update-rc.d awsfw_dns defaults
 
 nsname=$(/etc/init.d/awsfw_dns start 2>/dev/null)
-echo $nsname | egrep -q "${name}-${farmversion}"
+echo $nsname | egrep -q "${name}.*\.${stackname}-${version}"
 shortname=$(echo $nsname|cut -d. -f1)
 domainname=$(echo $nsname|cut -d. -f2-)
 
@@ -26,8 +28,10 @@ hostname $shortname
 
 cat << EOF >> /etc/dhcp/dhclient.conf
 interface "eth0" {
-    supersede domain-name ${domainname}
+    supersede domain-name "${domainname}";
 }
 EOF
 
-#/tmp/awsfw_standalone
+/etc/init.d/networking force-reload
+
+/tmp/awsfw_standalone
