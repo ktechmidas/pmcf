5a2a40389ae96e9e082fb355ee132a7b8abd1a4e
Stephen Gran <stephen.gran@piksel.com>
2014-07-14 12:18:42 +0100

    add test suite for strategy layer
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

---
+++ tests/unit/strategy/test_bluegreen.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2014 Piksel
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from nose.tools import assert_equals
+
+from pmcf.strategy import BlueGreen
+
+
+class TestBlueGreenStrategy(object):
+
+    def test_should_update_false(self):
+        strategy = BlueGreen()
+        assert_equals(strategy.should_update('update'), False)
+
+    def test_should_prompt_update_false(self):
+        strategy = BlueGreen()
+        assert_equals(strategy.should_prompt('update'), False)
+
+    def test_should_prompt_delete_false(self):
+        strategy = BlueGreen()
+        assert_equals(strategy.should_prompt('delete'), False)
