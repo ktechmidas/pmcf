0397719680b65347a808c60e0f03d05b34fff40f
Stephen Gran <stephen.gran@piksel.com>
2015-06-27 22:10:10 +0100

    run_tests.sh should preserve existing behavior
    Signed-off-by: Stephen Gran <stephen.gran@piksel.com>

--- tools/install_venv.sh
+++ tools/install_venv.sh
@@ -31,10 +31,10 @@ do
 done
 
 if [ "${force}" = yes ];then
-	rmvirtualenv pmcf
-	rm -rf doc/build
+	rm -rf .venv doc/build
 fi
 
-mkvirtualenv pmcf
-workon pmcf
-python setup.py install
+virtualenv -q .venv
+tools/with_venv.sh pip install --upgrade 'pip>=1.4'
+tools/with_venv.sh pip install --upgrade setuptools
+tools/with_venv.sh pip install --upgrade -r ${PWD}/requirements.txt -r ${PWD}/test-requirements.txt
